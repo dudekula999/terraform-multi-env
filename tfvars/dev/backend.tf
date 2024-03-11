3a5a35fdb5f081e249bf3d0769a0a5df571c5b41
dudekula999 <dastagiri160689@gmail.com>
2024-03-12 04:05:32 +0530

    first commit

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "reeda-remote-state-dev"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "reeda-remote-locking-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "reeda-remote-state-prod"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "reeda-remote-locking-prod"
